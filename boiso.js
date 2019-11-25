d71e46f4eacddfca4c6ace75bce1d235e05fde74
hoanglv92 <hoanglv92@gmail.com>
2019-11-25 22:09:03 +0700

    cập nhật bội 2 số

--- boiso.js
+++ boiso.js
@@ -1,12 +1,12 @@
 function boiso() {
-    var a=prompt('nhập vào số a');
+    let a=prompt('nhập vào số a');
     if (a==null){
         document.getElementById("hienthi").innerHTML="bạn đã bấm nút hủy";
         else{
             document.getElementById("hienthi").innerHTML="bạn vưa nhập số a là="+a;
         }
     }
-    var b=prompt("nhập vào số b");
+    let b=prompt("nhập vào số b");
     if (b==null){
         document.getElementById("hienthi").innerHTML="bạn đã bấm nút hủy";
         else{
